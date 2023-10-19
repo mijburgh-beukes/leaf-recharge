5b107aeb6bb6abf355f3358435a31663953f3930
EL\Mijburgh <Mijburgh@energyline.net>
2023-10-20 12:02:15 +1300

    fix: update calculator function

--- computed/leafRechargeTime.ts
+++ computed/leafRechargeTime.ts
@@ -3,9 +3,11 @@ const onePercentWatts = batteryCapacity / 100;
 const eightyPercentWatts = onePercentWatts * 80;
 const chargeRatePerHour = 6.6;
 
-export const timeTo80pc = (charge: number): string => {
-  const wattsTo80pc = eightyPercentWatts - charge * eightyPercentWatts;
+const timeTo80pc = (chargePC: number): string => {
+  const wattsTo80pc = eightyPercentWatts - chargePC * onePercentWatts;
   const hoursTo80pc = wattsTo80pc / chargeRatePerHour;
   const minsTo80pc = hoursTo80pc * 60;
-  return minsTo80pc.toString();
+  const hours = Math.floor(minsTo80pc / 60);
+  const mins = Math.floor(minsTo80pc - hours * 60);
+  return `${hours}h ${mins} mins`;
 };
